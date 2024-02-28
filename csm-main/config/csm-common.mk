4a29af2949b1ff994fcb1ec626d4257599e553ec
Wilson Radadia <159131702+WilsonRadadia20@users.noreply.github.com>
2024-02-28 15:43:37 +0530

    Update file.txt: Fix bug

--- csm-main/config/csm-common.mk
+++ csm-main/config/csm-common.mk
@@ -1,5 +1,5 @@
 # Common base image for all CSM images. When base image is upgraded, update the following 3 lines with URL, Version, and DEFAULT_BASEIMAGE variable.
 # URL: https://catalog.redhat.com/software/containers/ubi9/ubi-micro/615bdf943f6014fa45ae1b58?architecture=amd64&image=56b9f97db7e4bede96526c22
-# Version: ubi9/ubi-micro 9.3-10
+# Version: ubi9/ubi-micro 9.3-13
 DEFAULT_BASEIMAGE="registry.access.redhat.com/ubi9/ubi-micro@sha256:b88902acf3073b61cb407e86395935b7bac5b93b16071d2b40b9fb485db2135d"
 DEFAULT_GOIMAGE="golang:1.22"
